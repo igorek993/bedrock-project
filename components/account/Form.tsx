bfb884bd1ec8c02c68cf1ee84f920fa3d946f135
Igor Zhukov <igorek993@gmail.com>
2024-11-16 13:19:43 +1100

    it works

--- components/account/Form.tsx
+++ components/account/Form.tsx
@@ -1,19 +1,31 @@
-import { useActionState } from "react";
-import { uploadFile } from "@/serverFunctions/account/account";
-import { SubmitButton } from "./SubmitButton";
+import { getPresignedUrl } from "@/serverFunctions/account/account";
 
 const initialState = { status: "", message: "" };
 
 export function UploadForm() {
-  const [state, formAction] = useActionState(uploadFile, initialState);
+  async function uploadFiles(files) {
+    const presignedUrl = await getPresignedUrl(files);
+    console.log(presignedUrl.url);
+    console.log(files.get("file"));
 
+    const fileUpload = await fetch(presignedUrl.url, {
+      method: "PUT",
+      body: files.get("file"),
+    });
+
+    console.log(fileUpload);
+  }
   return (
     <div className="form-wrapper">
-      <form action={formAction}>
-        <input type="file" id="file" name="file" accept="*" multiple />
-        <SubmitButton />
+      <form action={uploadFiles}>
+        <input type="file" id="files" name="files" accept="*" multiple />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          upload
+        </button>
       </form>
-      {state?.status && <div className="bg-orange-300">{state?.message}</div>}
     </div>
   );
 }
--- serverFunctions/account/account.tsx
+++ serverFunctions/account/account.tsx
@@ -1,9 +1,27 @@
 "use server";
 
-export async function uploadFile(prevState, formData) {
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const client = new S3Client({
+  region: process.env.AWS_REGION,
+});
+
+export async function getPresignedUrl(files) {
   try {
-    console.log(formData);
-    return { status: "success", message: "File has been uploaded" };
+    const fileType = files.get("files")["type"];
+    const fileName = files.get("files")["name"];
+    console.log(fileName, fileType);
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: fileName,
+      ContentType: fileType,
+    });
+
+    const url = await getSignedUrl(client, command, { expiresIn: 60 });
+
+    return { url: url, status: "success", message: "File has been uploaded" };
   } catch (error) {
     return { status: "error", message: "Error" };
   }
