3dc8ec699f90f24a96a94c45bbea32e522b4f448
Igor Zhukov <igorek993@gmail.com>
2024-12-01 16:05:04 +1100

    infra add amplify roles

--- infra/amplify.tf
+++ infra/amplify.tf
@@ -3,7 +3,7 @@ resource "aws_amplify_app" "project_fufel" {
   name                 = "project-fufel"
   platform             = "WEB_COMPUTE"
   repository           = "https://github.com/igorek993/bedrock-project"
-  iam_service_role_arn = "arn:aws:iam::992382412567:role/service-role/AmplifySSRLoggingRole-f6aae01a-205e-4e89-b636-df5dbc54f4d7"
+  iam_service_role_arn = aws_iam_role.project_fufel_amplify_role.arn
 
   build_spec = <<-EOT
     version: 1
@@ -31,4 +31,98 @@ resource "aws_amplify_app" "project_fufel" {
     target = "/index.html"
 
   }
+}
+
+resource "aws_iam_policy" "project_fufel_logging_policy" {
+  description = null
+  name        = "AmplifySSRLoggingPolicy-f6aae01a-205e-4e89-b636-df5dbc54f4d7"
+  name_prefix = null
+  path        = "/service-role/"
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = [
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+          ]
+          Effect   = "Allow"
+          Resource = "arn:aws:logs:${local.region}:${local.account_id}:log-group:/aws/amplify/*:log-stream:*"
+          Sid      = "PushLogs"
+        },
+        {
+          Action   = "logs:CreateLogGroup"
+          Effect   = "Allow"
+          Resource = "arn:aws:logs:${local.region}:${local.account_id}:log-group:/aws/amplify/*"
+          Sid      = "CreateLogGroup"
+        },
+        {
+          Action   = "logs:DescribeLogGroups"
+          Effect   = "Allow"
+          Resource = "arn:aws:logs:${local.region}:${local.account_id}:log-group:*"
+          Sid      = "DescribeLogGroups"
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  tags     = {}
+  tags_all = {}
+}
+
+resource "aws_iam_role" "project_fufel_amplify_role" {
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "amplify.amazonaws.com"
+          }
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+
+  description           = "The service role that will be used by AWS Amplify for Web Compute app logging."
+  force_detach_policies = false
+  max_session_duration  = 3600
+  name                  = "AmplifySSRLoggingRole-f6aae01a-205e-4e89-b636-df5dbc54f4d7"
+  name_prefix           = null
+  path                  = "/service-role/"
+  permissions_boundary  = null
+  tags                  = {}
+  tags_all              = {}
+}
+
+
+resource "aws_iam_role_policy_attachment" "project_fufel_role_logging_policy" {
+  role       = aws_iam_role.project_fufel_amplify_role.name
+  policy_arn = aws_iam_policy.project_fufel_logging_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "project_fufel_role_app_policy" {
+  role       = aws_iam_role.project_fufel_amplify_role.name
+  policy_arn = aws_iam_policy.project_fufel_app_policy.arn
+}
+
+resource "aws_iam_policy" "project_fufel_app_policy" {
+  name = "project-fufel-app-policy"
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = [
+            "s3:*",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+          Sid      = "s3Access"
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
 }
---
+++ infra/imports.tf
@@ -0,0 +1,6 @@
+# import {
+#   to = aws_iam_policy.project_fufel_logging_policy
+#   id = "arn:aws:iam::992382412567:policy/service-role/AmplifySSRLoggingPolicy-f6aae01a-205e-4e89-b636-df5dbc54f4d7"
+# }
+
+#% terraform import aws_iam_role.project_fufel_amplify_role arn:aws:iam::992382412567:role/service-role/AmplifySSRLoggingRole-f6aae01a-205e-4e89-b636-df5dbc54f4d7
