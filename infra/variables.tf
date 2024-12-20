1de60238c0f5c9852854eff32fce2f7d8425a2df
Igor Zhukov <igorek993@gmail.com>
2024-12-21 00:46:42 +1100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,4 @@
 variable "user_ids" {
   type    = list(any)
-  default = ["user_2pe2Gzy2xnwd9kT3rpirWdFQzQH", "user_2qQy91pGHVvM3HQ6TMRpaVcWXu9"]
+  default = ["user_2pe2Gzy2xnwd9kT3rpirWdFQzQH", "user_2qQy91pGHVvM3HQ6TMRpaVcWXu9", "user_2qTzhYsJ7vh7nFZA7qux9GA0d32"]
 }
